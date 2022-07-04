a4b06e5e3b36ea04b605630a934fb4fb2b6853d4
Smikey <smikeywork@gmail.com>
2022-07-04 18:30:19 +0545

    :bulb: Print practice

---
+++ day2.py
@@ -0,0 +1,15 @@
+# print('I\'m beautiful')
+
+# print("First line ")
+# print("Second line ")
+
+print("First line\tSecond line ")
+print("********************************")
+print("********************************")
+print("First line\nSecond line ")
+
+print("Kritik\bka")
+print("This is back-slash\\")
+
+# this is bealutiful /\/\/\/\  mountain 
+
